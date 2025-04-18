8c3731f45123bc38c07b8304d11e9ee9895d8b96
fervh18 <112830242+fervh18@users.noreply.github.com>
2025-04-18 13:52:49 -0600

    Update st_functions.py

--- st_functions.py
+++ st_functions.py
@@ -6,37 +6,14 @@ def load_css():
     st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 def st_button(icon, url, label, iconsize):
-    if icon == 'youtube':
+    if icon == 'spotify':
         button_code = f'''
         <p>
-            <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
-                <svg xmlns="http://www.w3.org/2000/svg" width={iconsize} height={iconsize} fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
-                    <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
-                </svg>  
-                {label}
-            </a>
-        </p>'''
-
-elif icon == 'spotify':
-        button_code = f'''
-        <p>
-            <a href="{url}" class="btn btn-outline-success btn-lg btn-block" type="button" aria-pressed="true">
+             <a href="{url}" class="btn btn-outline-success btn-lg btn-block" type="button" aria-pressed="true">
                 <svg xmlns="http://www.w3.org/2000/svg" width="{iconsize}" height="{iconsize}" fill="currentColor" class="bi bi-spotify" viewBox="0 0 16 16">
                     <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.669 11.055a.532.532 0 0 1-.73.176 6.531 6.531 0 0 0-6.078-.37.532.532 0 0 1-.442-.97 7.595 7.595 0 0 1 7.064.43.532.532 0 0 1 .186.734zm.993-2.014a.665.665 0 0 1-.91.22 8.717 8.717 0 0 0-8.122-.494.665.665 0 1 1-.554-1.21 10.048 10.048 0 0 1 9.353.57.665.665 0 0 1 .233.914zm.112-2.055a.797.797 0 0 1-1.09.264 10.892 10.892 0 0 0-10.15-.6.797.797 0 1 1-.64-1.462 12.389 12.389 0 0 1 11.526.686.797.797 0 0 1 .354 1.112z"/>
                 </svg>  
                 {label}
-            </a>
-        </p>'''
-    
-    else:
-        button_code = f'''
-        <p>
-            <a href="{url}" class="btn btn-outline-secondary btn-lg btn-block" type="button" aria-pressed="true">
-                {label}
-            </a>
-        </p>'''
-
-    return button_code
     
     elif icon == 'twitter':
         button_code = f'''
