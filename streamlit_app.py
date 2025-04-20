c9ffc37c6b1a84b95b541721fc6026864df753df
fervh18 <112830242+fervh18@users.noreply.github.com>
2025-04-19 23:16:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,12 +18,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 icon_size = 20
 
-st.markdown("""
-<div style="background-color: ##4682B4; padding: 10px; border-radius: 5px; text-align: center; color: #FFFFF0;">
-    <strong>otro domingo de bajón pensando tanto :( ↓</strong>
-</div>
-""", unsafe_allow_html=True)
-icon_size = 20
+
 
 st.markdown("""
 <div style="background-color: #008080; padding: 10px; border-radius: 5px; text-align: center; color: #FFFFF0;">
