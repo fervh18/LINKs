113f850dc6a9e48c37440315da2679483babbd80
fervh18 <112830242+fervh18@users.noreply.github.com>
2025-04-19 22:50:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.markdown("""
     <strong>bienvenidx a tus playlists favoritas</strong>
 </div>
 """, unsafe_allow_html=True)
-icon_size = 20
+icon_size = 10
 
 st.markdown(st_button('spotify', 'https://open.spotify.com/playlist/5Zm4gmdf1MK1HulF6Qb5Hq?si=x7ZF9g1ESXCQ84nYsYO_GA', 'Escuchar en Spotify', 30), unsafe_allow_html=True)
 st.markdown(st_button('applemusic',
